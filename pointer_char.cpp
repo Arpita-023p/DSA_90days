f9942350ea6e6f99fab4e316878a55f88534439a
Arpita Panda <162110210+Arpita-023p@users.noreply.github.com>
2026-02-13 11:09:13 +0530

    Add files via upload
    charter pointer for practice

---
+++ pointer_char.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,4,5,6,7};
+    char ch[6]="abcde";
+    cout<<arr<<endl;
+    cout<<ch<<endl;
+
+    char *c=&ch[0];
+    cout<<c<<endl;
+
+    char temp='z';
+    char *p=&temp;
+    cout<<p<<endl;
+    return 0;
+}
